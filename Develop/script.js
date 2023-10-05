858f7ebea813b59f00b981f629ff12eb5c7b0a8a
Matthew_Hill <hillmatt58@gmail.com>
2023-10-05 08:14:19 -0600

    Added event listener

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var plength = []
+var passwordChar = [];
+var results = [];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,20 +13,35 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword () {
+ var userInput = Number(prompt('How many characters should your password have?'));
+ console.log("USER INPUT: ", userInput);
+ var lowerChecker = confirm("Lower");
+ if (lowerChecker) {
+  passwordChar.push(lowerCase)
+ }
 
-// Add function: when user clicks "generate password" button,
-// then page displays a series of prompts to choose from: 
-// 
-
-const generate = document.addEventListener('generate');
-  function plength() {
+ for (let i = 0; i < userInput; i++) {
+  results.push(lowerCase[Math.floor(Math.random() * lowerCase.length)])
+ }
+ console.log("RESULTS: ", results);
+ return results.
  
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
 
-const userInput = prompt('How many characters should your password have?');
-  if (userInput !== null) {
+// Add function: when user clicks "generate password" button,
+// then page displays a series of prompts to choose from: 
+  function displayprompt() {
+    
+  }
+  
 
-  } 
+// const generate = document.addEventListener('generate');
+//   function plength() {
+ 
+// }
+
+
