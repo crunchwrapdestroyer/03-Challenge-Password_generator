66a31f410057d9ad924635099909171acd40de05
Matthew_Hill <hillmatt58@gmail.com>
2023-10-05 12:03:38 -0600

    display prompt

--- Develop/script.js
+++ Develop/script.js
@@ -13,31 +13,20 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function generatePassword () {
- var userInput = Number(prompt('How many characters should your password have?'));
- console.log("USER INPUT: ", userInput);
- var lowerChecker = confirm("Lower");
- if (lowerChecker) {
-  passwordChar.push(lowerCase)
- }
-
- for (let i = 0; i < userInput; i++) {
-  results.push(lowerCase[Math.floor(Math.random() * lowerCase.length)])
- }
- console.log("RESULTS: ", results);
- return results.
- 
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
 
 // Add function: when user clicks "generate password" button,
 // then page displays a series of prompts to choose from: 
-  function displayprompt() {
-    
-  }
-  
-
-// const generate = document.addEventListener('generate');
-//   function plength()
+  function generatePassword () {
+ var userInput = Number(prompt('How many characters should your password have?'));
+ console.log("USER INPUT: ", userInput);
+ var lowerChecker = confirm("Lower");
+ if (lowerChecker) {
+  passwordChar.push(lowerCase)}
+ for (let i = 0; i < userInput; i++) {
+  results.push(lowerCase[Math.floor(Math.random() * lowerCase.length)])
+}
+ console.log("RESULTS: ", results);
+ return results;
+ }
